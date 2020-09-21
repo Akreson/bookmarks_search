541b88858c922ef8b5046575f1671a4f7b0fd7cf
Alex Boot <redactae@gmail.com>
2020-09-21 18:43:53 +0300

    Start add .html parsing

--- bookmarks_search.py
+++ bookmarks_search.py
@@ -3,6 +3,7 @@ import sys
 import json
 import argparse
 import urllib.request
+from browser_bm_parser import *
 from speed_dial_parser import *
 
 JSON_EXT = '.json'
@@ -22,8 +23,8 @@ class SearchData:
 
     def get_file_group_url_info(self, file_desc, file_ext):
         if file_ext == HTML_EXT:
-            print(HTML_EXT)
-            file_urls_group = None
+            file_data = file_desc.read()
+            file_urls_group = browser_bm_parse_html(self.exclude_group, self.url_group, self.urls, file_data)
         elif file_ext == JSON_EXT:
             file_data = json.load(file_desc)
             file_urls_group = sd_parse_json(self.exclude_group, self.url_group, self.urls, file_data)
@@ -47,7 +48,7 @@ class SearchData:
             if group.name not in self.exclude_group:
                 curr_len = len(self.urls)
                 group_min = curr_len
-                group_max = group.min + len(group.ulrs)
+                group_max = group_min + len(group.urls)
                 url_group = UrlGroup(group.name, group_min, group_max)
                 self.url_group.append(url_group)
                 self.urls += group.urls
@@ -64,7 +65,10 @@ class SearchData:
 
         self.pack_to_search_data(files_groups)
 
-    def parse_files(self, files):
+    def parse_files(self, files, exclude_groups):
+        if exclude_groups:
+            self.exclude_group = list(exclude_groups)
+            
         files_to_parse = []
 
         for file in files:
@@ -92,16 +96,18 @@ def parse_cmd_args():
         help='string for search in url links')
     cli_parser.add_argument('-g', '--group', action='store', nargs='+',
         help='group of bookmarks in provided file')
+    cli_parser.add_argument('-ex', '--exclude', action='store', nargs='+',
+        help='exclude group from search')
 
     cli_args = cli_parser.parse_args()
     return cli_args
 
 
 def main():
-    search_state = SearchData()
+    search_data = SearchData()
     cli_args = parse_cmd_args()
 
-    search_state.parse_files(cli_args.file)
+    search_data.parse_files(cli_args.file, cli_args.exclude)
 
     print(cli_args)
 
---
+++ browser_bm_parser.py
@@ -0,0 +1,103 @@
+from urllib import parse
+from html.parser import HTMLParser
+from file_urls_parser import *
+
+class Tag:
+    DT = 'dt'
+    DL = 'dl'
+    H3 = 'h3'
+    H1 = 'h1'
+    A = 'a'
+    P = 'p'
+
+class ParseState:
+    Empty = 0 
+    GroupData = 1
+    GroupData_Name = 2
+    GroupData_Links = 3
+
+    ExitGroup_Name = 4
+    EnterGroupData = 5
+    ExitGroupData = 6
+
+    SubgroupData = 7
+    SubgroupData_Name = 8
+
+    InitGroup = 9
+    InitSubGroup = 10
+
+    DataLink = 11
+
+class BookmarksParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.file_url_groups = []
+        self.curr_group = None
+        self.state = ParseState.Empty
+
+        self.prev_open_tag = None
+        self.prev_end_tag = None
+
+        self.open_tag = None
+        self.end_tag = None
+
+        self.expected_tag = None
+        self.curr_tag_data = None
+        self.curr_subgroup_name = ''
+    
+    def handle_endtag(self, tag):
+        if self.end_tag:
+            self.prev_end_tag = self.end_tag
+            self.end_tag = tag
+        else:
+            self.end_tag = tag
+            
+#TODO: Finish parsing
+        if tag == Tag.H3:
+            if self.state == ParseState.GroupData_Name:
+                self.curr_group = FileUrlGroup(self.curr_subgroup_name + self.curr_tag_data)
+                print(self.curr_group.name)
+            elif self.state == ParseState.InitSubGroup:
+                self.curr_subgroup_name = self.curr_group.name + '.'
+        elif tag == Tag.A and self.state == ParseState.DataLink:
+            pass
+        elif tag == Tag.DL and self.state == ParseState.DataLink:
+            self.file_url_groups.append(self.curr_group)
+            self.curr_subgroup_name = ''
+            self.state = ParseState.InitGroup
+
+    def handle_starttag(self, tag, attrs):
+        if self.open_tag:
+            self.prev_open_tag = self.open_tag
+            self.open_tag = tag
+        else:
+            self.open_tag = tag
+
+        if tag == Tag.DT:
+            if self.state == ParseState.InitGroup:
+                self.state = ParseState.GroupData
+        elif tag == Tag.H3:
+            if self.state == ParseState.GroupData: 
+                self.state = ParseState.GroupData_Name
+            elif self.state == ParseState.GroupData_Links and self.prev_open_tag == Tag.DT:
+                self.state == ParseState.InitSubGroup
+        elif tag == Tag.DL:
+            if self.end_tag == Tag.H1:
+                self.state = ParseState.InitGroup
+            if self.state == ParseState.GroupData_Name:
+                self.state = ParseState.GroupData_Links
+            else:
+                self.state = ParseState.Empty
+        elif tag == Tag.A and self.prev_open_tag == Tag.DT:
+            self.state = ParseState.DataLink
+        
+    def handle_data(self, data):
+       self.curr_tag_data = data
+
+def browser_bm_parse_html(exlcude_group, url_group, urls, html_data):
+    bm_parser = BookmarksParser()
+    bm_parser.feed(html_data)
+    print(len(bm_parser.file_url_groups))
+
+    return bm_parser.file_url_groups
+
